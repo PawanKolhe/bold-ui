60519c3fd4d814237beba8d2a6645ad206ccebc2
Pawan Kolhe <pawanashishkolhe@gmail.com>
2023-05-27 10:33:26 +0600

    chore: update scripts

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@color-ui/react"],
-  experimental: {
-    typedRoutes: true,
-  },
 };
 
 module.exports = nextConfig;
